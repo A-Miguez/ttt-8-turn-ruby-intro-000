aef3a2dafc1f2fa683ca5bfd20e4c17fc89569c0
Anthony Miguez <anm501@stern.nyu.edu>
2020-05-10 02:37:50 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,40 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+=begin
+def input_to_index(input)
+  input = input.to_i
+  index = input - 1
+  return index
+end
+=end
+
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+
+def valid_move?(board,index)
+  if index.between?(0,8) && !position_taken?(board,index)
+    return true
+  else
+    return false
+  end
+end
+
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
